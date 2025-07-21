92a9e775ff2aaa8186a6b93dc7ba258c267535f6
Mohamed74Nassser <nasser.nasser1909@gmail.com>
2025-07-22 02:14:17 +0300

    Move dashboard package files to repo root

--- packages/nasser/dashboard/src/resources/views/components/auth-session-status.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['status', 'class' => ''])
-
-@if ($status)
-    <div {{ $attributes->merge(['class' => 'alert alert-success alert-dismissible ' . $class]) }}>
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        <i class="bi bi-check-circle me-2"></i>
-        {{ $status }}
-    </div>
-@endif 
--- packages/nasser/dashboard/src/resources/views/components/input-label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value', 'for'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }} for="{{ $for }}">
-    {{ $value ?? $slot }}
-</label> 
