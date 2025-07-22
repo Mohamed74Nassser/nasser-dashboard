beb09fcb6569a42898f29e0fb081baeff1561ab7
Mohamed Nasser <57974869+Mohamed74Nassser@users.noreply.github.com>
2025-07-22 03:28:58 +0300

    Update DashboardServiceProvider.php

--- src/Providers/DashboardServiceProvider.php
+++ src/Providers/DashboardServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class DashboardServiceProvider extends ServiceProvider
 {
-    public function boot()
+   /* public function boot()
     {
         // Load views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'nasser-dashboard');
@@ -20,6 +20,20 @@ class DashboardServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
         ], 'nasser-dashboard-assets');
+    }
+    */
+    public function boot()
+    {
+        // دي عشان نعرف إذا Laravel شاف الباكدج فعلاً
+        \Log::info('[NASSER_DASHBOARD] DashboardServiceProvider booted.');
+    
+        $this->publishes([
+            __DIR__.'/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
+        ], 'nasser-dashboard-assets');
+    
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/nasser-dashboard'),
+        ], 'nasser-dashboard-views');
     }
 
     public function register()
