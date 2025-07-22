27ae088828f78cba06c68b76c4bfd548ca1c4432
Mohamed Nasser <57974869+Mohamed74Nassser@users.noreply.github.com>
2025-07-22 04:30:40 +0300

    Update head.blade.php

--- src/resources/views/layouts/head.blade.php
+++ src/resources/views/layouts/head.blade.php
@@ -29,7 +29,7 @@
     />
     <!--end::Third Party Plugin(Bootstrap Icons)-->
     <!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="{{ asset('assets/css/adminlte.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/nasser-dashboard/assets/css/adminlte.css') }}" />
 
     <!--end::Required Plugin(AdminLTE)-->
     
