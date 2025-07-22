1729645d5165e860e9f329176dee639ae1b0f0b9
Mohamed Nasser <57974869+Mohamed74Nassser@users.noreply.github.com>
2025-07-22 03:35:10 +0300

    Update DashboardServiceProvider.php

--- src/Providers/DashboardServiceProvider.php
+++ src/Providers/DashboardServiceProvider.php
@@ -6,34 +6,20 @@ use Illuminate\Support\ServiceProvider;
 
 class DashboardServiceProvider extends ServiceProvider
 {
-   /* public function boot()
+    public function boot()
     {
-        // Load views
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'nasser-dashboard');
-
-        // Publish assets
-        $this->publishes([
-            __DIR__.'/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
-        ], 'nasser-dashboard-assets');
+        // Load views from package
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'nasser-dashboard');
 
         // Publish views
         $this->publishes([
-            __DIR__.'/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
-        ], 'nasser-dashboard-assets');
-    }
-    */
-    public function boot()
-    {
-        // دي عشان نعرف إذا Laravel شاف الباكدج فعلاً
-        \Log::info('[NASSER_DASHBOARD] DashboardServiceProvider booted.');
-    
-        $this->publishes([
-            __DIR__.'/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
-        ], 'nasser-dashboard-assets');
-    
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/nasser-dashboard'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/nasser-dashboard'),
         ], 'nasser-dashboard-views');
+
+        // Publish assets
+        $this->publishes([
+            __DIR__ . '/../public/assets' => public_path('vendor/nasser-dashboard/assets'),
+        ], 'nasser-dashboard-assets');
     }
 
     public function register()
